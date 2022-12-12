bae55f4e459b73c0bf3d32fe37c39179008e0961
Mon Macbook <wendykrwn@gmail.com>
2022-12-12 17:39:39 +0100

    entreprise building

---
+++ app/entreprise/page.jsx
@@ -0,0 +1,43 @@
+import Image from "next/image"
+import bureau from "/public/salles/Bureau.png"
+import cafet from '/public/salles/Cantine.png'
+import openspace from '/public/salles/Hotel.png'
+import kitchen from '/public/salles/Shop.png'
+import reunion from '/public/salles/Ecole.png'
+// import  from '/public/salles/Cantine.png';
+export default function Page({ }) {
+  return (
+    <div className="container p-6 bg-yellow-400">
+      <div className="px-[30px]">
+        <div className="h-[20px] w-full bg-white"/>
+      </div>
+      <div className="px-[40px]">
+        <div className="bg-[#D9D9D9] grid grid-cols-6 roof w-full px-3 pt-3">
+          <div/>
+          <div/>
+          <div/>
+          <div/>
+          <div/>
+          <div/>
+        </div>
+      </div>
+      <div className="company grid grid-cols-6 grid-rows-3 bg-[#D9D9D9] p-3 px-10 ">
+        <div className="col-span-3">
+          <Image src={bureau} alt="bureau.png" />
+        </div>
+        <div className="col-span-3">
+          <Image src={reunion} alt="openspace.png" />
+        </div>
+        <div className="col-span-6">
+          <Image src={openspace} alt="openspace.png" />
+        </div>
+        <div className="col-span-3">
+          <Image src={kitchen} alt="cuisine.png" />
+        </div>
+        <div className="col-span-3">
+          <Image src={cafet} alt="openspace.png" contain />
+        </div>
+      </div>
+    </div>
+  )
+ }
