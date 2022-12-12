9a78d82d3f4d47961ebac94ff79c547590c1b7ab
Abdelilah Lezar <abdelilahlezar@gmail.com>
2022-12-12 07:43:49 -0800

    Working on "Consignes"

--- app/consignes/[materiel]/page.jsx
+++ app/consignes/[materiel]/page.jsx
@@ -1,3 +1,18 @@
+import styles from '../../../styles/consignes.css'
+import Image from 'next/image'
+import logo from '@/assets/images/Verre.png'
 export default function Page({}) {
-  return <div>Page</div>;
+    return <div className="consignes">
+        <div id="rectangle">
+            <div className="title-container">
+                <h1 className="title text-center">HAHA</h1>
+            </div>
+            <div id="circle"></div>
+            <Image id="logo" src={logo} width="150" height="150"/>
+        </div>
+        <div className="section-1">
+            {/*<Image src={logo}/>*/}
+        </div>
+
+    </div>;
 }
