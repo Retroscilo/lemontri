3446a2e667b0be0f4a4a3578d0f92e0a39173ffc
Mon Macbook <wendykrwn@gmail.com>
2022-12-13 19:00:15 +0100

    afficher les astuces en fonction des salles

--- app/entreprise/page.jsx
+++ app/entreprise/page.jsx
@@ -40,26 +40,74 @@ export default function Page({ }) {
     },
   ]
 
-  const astuces = [
+  const tricks = [
     {
       room: "bureau",
       text: "25% des documents sont jetés 5 minutes après leur impréssion ! Pour limiter le gaspillage, pensez à limiter le nombre d’imprimantes et à bien les paramétrer pour qu'elles impriment en recto-verso par défaut. Installer des bac à brouillons pour récupérer les impréssions et pensez à utiliser du papier recyclé !",
+    },
+    {
+      room: "bureau",
+      text: "Bureau 2 hehe 25% des documents sont jetés 5 minutes après leur impréssion ! Pour limiter le gaspillage, pensez à limiter le nombre d’imprimantes et à bien les paramétrer pour qu'elles impriment en recto-verso par défaut. Installer des bac à brouillons pour récupérer les impréssions et pensez à utiliser du papier recyclé !",
+    },
+    {
+      room: "bureau",
+      text: "Bureau 3 hehe 25% des documents sont jetés 5 minutes après leur impréssion ! Pour limiter le gaspillage, pensez à limiter le nombre d’imprimantes et à bien les paramétrer pour qu'elles impriment en recto-verso par défaut. Installer des bac à brouillons pour récupérer les impréssions et pensez à utiliser du papier recyclé !",
+    },
+    {
+      room: "bureau",
+      text: "Bureau 4 hehe 25% des documents sont jetés 5 minutes après leur impréssion ! Pour limiter le gaspillage, pensez à limiter le nombre d’imprimantes et à bien les paramétrer pour qu'elles impriment en recto-verso par défaut. Installer des bac à brouillons pour récupérer les impréssions et pensez à utiliser du papier recyclé !",
     },
     {
       room: "cuisine",
       text: "cuisine ipsum dolor sit amet consectetur adipisicing elit. At veniam fugit labore! Doloribus, reiciendis! Aut consequatur iusto in aperiam error sapiente, sit, cupiditate odio nemo incidunt illum eos recusandae consequuntur?",
     },
     {
+      room: "cuisine",
+      text: "cuisine2 ipsum dolor sit amet consectetur adipisicing elit. At veniam fugit labore! Doloribus, reiciendis! Aut consequatur iusto in aperiam error sapiente, sit, cupiditate odio nemo incidunt illum eos recusandae consequuntur?",
+    },
+    {
+      room: "cuisine",
+      text: "cuisine3 ipsum dolor sit amet consectetur adipisicing elit. At veniam fugit labore! Doloribus, reiciendis! Aut consequatur iusto in aperiam error sapiente, sit, cupiditate odio nemo incidunt illum eos recusandae consequuntur?",
+    },
+    {
       room: "cafetaria",
       text: "osjogijipgs pofjsjd!",
+    },
+    {
+      room: "cafetaria",
+      text: "cafetaria2 osjogijipgs pofjsjd!",
+    },
+    {
+      room: "cafetaria",
+      text: "cafetaria3 osjogijipgs pofjsjd!",
+    },
+    {
+      room: "cafetaria",
+      text: "cafetaria4 osjogijipgs pofjsjd!",
     },
     {
       room: "openspace",
       text: "openspace 25% des documents sont jetés 5 minutes après leur impréssion ! Pour limiter le gaspillage, pensez à limiter le nombre d’imprimantes et à bien les paramétrer pour qu'elles impriment en recto-verso par défaut. Installer des bac à brouillons pour récupérer les impréssions et pensez à utiliser du papier recyclé !",
     },
     {
+      room: "openspace",
+      text: "openspace2 25% des documents sont jetés 5 minutes après leur impréssion ! Pour limiter le gaspillage, pensez à limiter le nombre d’imprimantes et à bien les paramétrer pour qu'elles impriment en recto-verso par défaut. Installer des bac à brouillons pour récupérer les impréssions et pensez à utiliser du papier recyclé !",
+    },
+    {
+      room: "openspace",
+      text: "openspace3 25% des documents sont jetés 5 minutes après leur impréssion ! Pour limiter le gaspillage, pensez à limiter le nombre d’imprimantes et à bien les paramétrer pour qu'elles impriment en recto-verso par défaut. Installer des bac à brouillons pour récupérer les impréssions et pensez à utiliser du papier recyclé !",
+    },
+    {
       room: "reunion",
-      text: "reu Lorem ipsum dolor sit amet consectetur adipisicing elit. At veniam fugit labore! Doloribus, reiciendis! Aut consequatur iusto in aperiam error sapiente, sit, cupiditate odio nemo incidunt illum eos recusandae consequuntur?",
+      text: "reu1 Lorem ipsum dolor sit amet consectetur adipisicing elit. At veniam fugit labore! Doloribus, reiciendis! Aut consequatur iusto in aperiam error sapiente, sit, cupiditate odio nemo incidunt illum eos recusandae consequuntur?",
+    },
+    {
+      room: "reunion",
+      text: "reu2 Lorem ipsum dolor sit amet consectetur adipisicing elit. At veniam fugit labore! Doloribus, reiciendis! Aut consequatur iusto in aperiam error sapiente, sit, cupiditate odio nemo incidunt illum eos recusandae consequuntur?",
+    },
+    {
+      room: "reunion",
+      text: "reu3 Lorem ipsum dolor sit amet consectetur adipisicing elit. At veniam fugit labore! Doloribus, reiciendis! Aut consequatur iusto in aperiam error sapiente, sit, cupiditate odio nemo incidunt illum eos recusandae consequuntur?",
     },
     {
       room: "cafetaria",
@@ -67,11 +115,15 @@ export default function Page({ }) {
     },
   ]
 
+  function getRandomInt(min, max) {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min + 1)) + min
+  }
   useEffect(() => {
-    let intRandom = Math.floor(Math.random() * astuces.length)
-    intRandom = intRandom > astuces.length ? intRandom - 1 : intRandom
+    const intRandom = getRandomInt(0,tricks.length - 1)
 
-    const trick = astuces[intRandom]
+    const trick = tricks[intRandom]
 
     setCurrentTrick(trick)
 
@@ -83,6 +135,13 @@ export default function Page({ }) {
     setCurrentTrick({})
   }
 
+  const handleClickRoom = (room) => {
+    const tricksRoom = tricks.filter(el => el.room == room)
+    const randomInt = getRandomInt(0,tricksRoom.length - 1)
+    console.log({ tricksRoom, randomInt })
+    setCurrentTrick(tricksRoom[randomInt])
+    setShowPopup(true)
+  }
 
   return (
     <div className="relative w-full">
@@ -101,9 +160,11 @@ export default function Page({ }) {
           {salles.map(({ name, src }) => (
             <div
               key={name}
+              onClick={()=>{handleClickRoom(name)}}
               className={`
               relative
               tooltip
+              cursor-pointer
               ${
                 name == currentTrick?.room
                   ? "border-primary border-[4px] active-room "
@@ -112,7 +173,7 @@ export default function Page({ }) {
                `}
             >
               <Image src={src} alt={`${name}.png`} />
-              <span className="tooltiptext">{name}</span>
+              <span className="tooltiptext ">{name}</span>
             </div>
           ))}
           {/* <div className="col-span-3">
