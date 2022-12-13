a6632972cb6141c3bd0aab1ce9f1b31b0b7cb034
Mon Macbook <wendykrwn@gmail.com>
2022-12-13 14:28:14 +0100

    popup sur mobile

--- app/entreprise/page.jsx
+++ app/entreprise/page.jsx
@@ -7,38 +7,70 @@ import reunion from '/public/salles/Ecole.png'
 import Popup from "@/components/popup"
 // import  from '/public/salles/Cantine.png';
 export default function Page({ }) {
+  const salles = [
+    {
+      name: "bureau",
+      src: bureau,
+      size: 3
+    },
+    {
+      name: "reunion",
+      src: reunion,
+      size: 3
+    },
+    {
+      name: "openspace",
+      src: openspace,
+      size: 6
+    },
+    {
+      name: "cuisine",
+      src: kitchen,
+      size: 3
+    },
+    {
+      name: "cafet",
+      src: cafet,
+      size: 3
+    },
+  ]
   return (
-    <div className="bg-pink-400">
-      <Popup />
+    <div className="relative w-full">
+      <div className="container p-6 bg-yellow-400">
+        <div className="px-[30px]">
+          <div className="h-[10px] w-full bg-white" />
+        </div>
+        <div className="px-[40px]">
+          <div className="bg-[#D9D9D9] grid grid-cols-6 roof w-full px-3 pt-3">
+            {[...Array(6).keys()].map((i) => (
+              <div key={i} />
+            ))}
+          </div>
+        </div>
+        <div className="company grid  bg-[#D9D9D9] p-3 px-10 ">
+          {salles.map(({ name, src }) => (
+            <div key={name}>
+              <Image src={src} alt={`${name}.png`} />
+            </div>
+          ))}
+          {/* <div className="col-span-3">
+            <Image src={bureau} alt="bureau.png" />
+          </div>
+          <div className="col-span-3">
+            <Image src={reunion} alt="openspace.png" />
+          </div>
+          <div className="col-span-6">
+            <Image src={openspace} alt="openspace.png" />
+          </div>
+          <div className="col-span-3">
+            <Image src={kitchen} alt="cuisine.png" />
+          </div>
+          <div className="col-span-3">
+            <Image src={cafet} alt="openspace.png" />
+          </div> */}
+        </div>
+      </div>
+      <Popup className="sticky bottom-0 left-0" />
     </div>
-    //   <div className="container p-6 bg-yellow-400">
-    //     <div className="px-[30px]">
-    //       <div className="h-[10px] w-full bg-white"/>
-    //     </div>
-    //     <div className="px-[40px]">
-    //       <div className="bg-[#D9D9D9] grid grid-cols-6 roof w-full px-3 pt-3">
-    //         {
-    //           [...Array(6).keys()].map(i=><div key={i}/>)
-    //         }
-    //       </div>
-    //     </div>
-    //     <div className="company grid grid-cols-6 grid-rows-3 bg-[#D9D9D9] p-3 px-10 ">
-    //       <div className="col-span-3">
-    //         <Image src={bureau} alt="bureau.png" />
-    //       </div>
-    //       <div className="col-span-3">
-    //         <Image src={reunion} alt="openspace.png" />
-    //       </div>
-    //       <div className="col-span-6">
-    //         <Image src={openspace} alt="openspace.png" />
-    //       </div>
-    //       <div className="col-span-3">
-    //         <Image src={kitchen} alt="cuisine.png" />
-    //       </div>
-    //       <div className="col-span-3">
-    //         <Image src={cafet} alt="openspace.png" />
-    //       </div>
-    //     </div>
-    //   </div>
   )
  }
--- components/popup.jsx
+++ components/popup.jsx
@@ -6,7 +6,7 @@ import megaphone from '/public/mascote/megaphone.png';
 const astucesBureau = [
   {
     salle: "bureau",
-    astuce: "25% des documents sont jetés 5 minutes après leur impréssion ! Pour limiter le gaspillage, pensez à limiter le nombre d’imprimantes et à bien les paramétrer pour qu'elles impriment en recto-verso par défaut. Installer des bac à brouillons pour récupérer les impréssions et pensez à utiliser du papier recyclé !"
+    astuce: "25% des documents sont jetés 5 minutes après leur impréssion ! Pour limiter le gaspillage, pensez à limiter le nombre d’imprimantes et à bien les paramétrer pour qu'elles impriment en recto-verso par défaut. Installer des bac à brouillons pour récupérer les impréssions et pensez à utiliser du papier recyclé !",
   },
   {
     salle: "cuisine",
@@ -19,35 +19,39 @@ const astucesBureau = [
 ]
 
 
-const Popup = ({ onClose, position }) => {
+const Popup = ({ onClose, position,className }) => {
   return (
-    <div className="tooltip bg-white rounded-lg w-[250px] h-[300px]">
-      <div className='text-right pr-1'>
-        <button className="text-[32px] inline-block text-right">
-          X
-        </button>
-      </div>
-      <div className="container w-full p-2 pb-1 flex flex-col justify-center">
-        <div className="flex relative">
-          <Image
-            src={megaphone}
-            width={30.65}
-            height={43.93}
-            alt="megaphone.svg"
-          />
-          <h1>Le saviez-vous</h1>
+    <div className={` container flex justify-center w-full ${className}`}>
+      <div className="tooltip relative bg-white rounded-none md:rounded-lg w-full p-5">
+        <div className="text-right top-1 right-2 absolute">
+          <button className="text-[32px] inline-block text-right">
+            &#x2715;
+          </button>
         </div>
-        <div className="h-[150px] max-6-lines">
-          25% des documents sont jetés 5 minutes après leur impréssion ! Pour
-          limiter le gaspillage, pensez à limiter le nombre d’imprimantes et à
-          bien les paramétrer pour qu'elles impriment en recto-verso par défaut.
-          Installer des bac à brouillons pour récupérer les impréssions et
-          pensez à utiliser du papier recyclé !
-        </div>
-        <div className="text-right">
-          <Link className="underline text-secondary-light" href={"#"}>
-            En savoir plus
-          </Link>
+        <div className="container w-full  flex flex-col justify-center">
+          <div className="flex relative items-center gap-x-2 -mt-5 h-[80px]">
+            <Image
+              src={megaphone}
+              style={{
+                height: "100%",
+                width: "auto",
+              }}
+              alt="megaphone.svg"
+            />
+            <h3 className="text-[20px]">Le saviez-vous ?</h3>
+          </div>
+          <div className="h-[100px] line-camp ">
+            25% des documents sont jetés 5 minutes après leur impréssion ! Pour
+            limiter le gaspillage, pensez à limiter le nombre d’imprimantes et à
+            bien les paramétrer pour qu'elles impriment en recto-verso par
+            défaut. Installer des bac à brouillons pour récupérer les
+            impréssions et pensez à utiliser du papier recyclé !
+          </div>
+          <div className="text-right">
+            <Link className="underline text-secondary-light" href={"#"}>
+              En savoir plus
+            </Link>
+          </div>
         </div>
       </div>
     </div>
