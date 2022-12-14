0c27c85ad5af55e820f57c9745b09152be59c71e
Mon Macbook <wendykrwn@gmail.com>
2022-12-14 16:43:31 +0100

    page d'acceuil

--- components/FileInput.jsx
+++ components/FileInput.jsx
@@ -1,31 +1,38 @@
-import { useRef } from "react";
-import { IconButton } from "@mui/material";
-import AddAPhotoIcon from "@mui/icons-material/AddAPhoto";
-import { CircularProgress } from "@mui/material";
-import { useState } from "react";
-const FileInput = ({ onChange }) => {
-  const fileInput = useRef(null);
-  const [loading, setLoading] = useState(false);
+"use client"
+import { useRef } from "react"
+import { IconButton } from "@mui/material"
+import AddAPhotoIcon from "@mui/icons-material/AddAPhoto"
+import { CircularProgress } from "@mui/material"
+import { useState } from "react"
+const FileInput = ({ onChange, className}) => {
+  const fileInput = useRef(null)
+  const [loading, setLoading] = useState(false)
   async function handleChange(e) {
-    setLoading(true);
-    await onChange(e);
-    setLoading(false);
+    setLoading(true)
+    if (onChange) await onChange(e)
+    setLoading(false)
   }
 
   return (
-    <div className="scale-150 bg-white rounded-full">
+    <div className={`scale-150 bg-white rounded-full ${className}`}>
       {!loading ? (
         <>
           <IconButton size={"large"} onClick={() => fileInput.current.click()}>
             <AddAPhotoIcon fontSize="80px" />
           </IconButton>
-          <input ref={fileInput} onChange={handleChange} type="file" accept="image/*" hidden />
+          <input
+            ref={fileInput}
+            onChange={handleChange}
+            type="file"
+            accept="image/*"
+            hidden
+          />
         </>
       ) : (
         <CircularProgress />
       )}
     </div>
-  );
-};
+  )
+}
 
-export default FileInput;
+export default FileInput
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,15 @@ module.exports = {
           light: "#069B3E",
           dark: "#408C32",
         }, // vert
+        light: "#F8F7D3", // jaune pale
         gray: "#969696",
         dark: "#4B4B4B",
       },
+    },
+    fontFamily: {
+      edosz: ["edosz", "serif"], // Ensure fonts with spaces have " " surrounding it.
+      roboto: ["Roboto", "sans-serif"],
+      bebaneue: ['"Beba Neue"', 'cursive'],
     },
   },
   plugins: [],
