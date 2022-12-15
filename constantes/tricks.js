96e122a71499103db0f8a481e5b690e9538e4c96
Merveil <nganongomerveil@gmail.com>
2022-12-15 15:04:12 +0100

    maj des astuces

--- constantes/tricks.js
+++ constantes/tricks.js
@@ -1,43 +1,53 @@
 export const tricks = [
   {
-    room: "bureau",
-    text: "25% des documents sont jetés 5 minutes après leur impréssion ! Pour limiter le gaspillage, pensez à limiter le nombre d’imprimantes et à bien les paramétrer pour qu'elles impriment en recto-verso par défaut. Installer des bac à brouillons pour récupérer les impréssions et pensez à utiliser du papier recyclé !",
-  },
-  {
-    room: "bureau",
-    text: "Bureau 2 hehe 25% des documents sont jetés 5 minutes après leur impréssion ! Pour limiter le gaspillage, pensez à limiter le nombre d’imprimantes et à bien les paramétrer pour qu'elles impriment en recto-verso par défaut. Installer des bac à brouillons pour récupérer les impréssions et pensez à utiliser du papier recyclé !",
-  },
-  {
-    room: "bureau",
-    text: "Bureau 3 hehe 25% des documents sont jetés 5 minutes après leur impréssion ! Pour limiter le gaspillage, pensez à limiter le nombre d’imprimantes et à bien les paramétrer pour qu'elles impriment en recto-verso par défaut. Installer des bac à brouillons pour récupérer les impréssions et pensez à utiliser du papier recyclé !",
-  },
-  {
-    room: "bureau",
-    text: "Bureau 4 hehe 25% des documents sont jetés 5 minutes après leur impréssion ! Pour limiter le gaspillage, pensez à limiter le nombre d’imprimantes et à bien les paramétrer pour qu'elles impriment en recto-verso par défaut. Installer des bac à brouillons pour récupérer les impréssions et pensez à utiliser du papier recyclé !",
-  },
-  {
+    matiere: "biodechets",
     room: "cuisine",
-    text: "cuisine ipsum dolor sit amet consectetur adipisicing elit. At veniam fugit labore! Doloribus, reiciendis! Aut consequatur iusto in aperiam error sapiente, sit, cupiditate odio nemo incidunt illum eos recusandae consequuntur?",
+    text: "Depuis le 1er janvier 2012, les personnes qui produisent ou détiennent une quantité importante de biodéchets ont l'obligation de tirer ces biodéchets et de les faire valoriser dans des filières adaptées.La généralisation de ce tri à la source est prévue d'ici 2025 pour tous les producteurs de déchets en France. Objectif: que chaque citoyen ait à sa disposition une solution lui permettant de ne pas jeter ses biodéchets dans les ordures ménagères résiduelles",
   },
   {
+    matiere: "bois",
+    room: "openspace",
+    text: "Les bois de la classe A,B ou AB sont recyclables et valorisables ce qui n'est pas le cas des bois de classe C. En effet, ces derniers ont reçu differents traitements chimiques indissociables du matériau rendant ainsi les bois impropres à une valorisation matière. Les bois de classe C doivent donc être incinées dans des installations spécifiques car les risques d'émissions polllauntes sont trop importants.",
+  },
+  {
+    matiere: "bouteilles en plastique",
+    room: "réunion",
+    text: "La loi relative à la lutte contre le gaspillage et à l'économie circulaire (AGEC) promulgée le 10 fevrier 2020 fixe objectif de taux de collecte des bouteilles plastiques de boisson 77% en 2025 et 90% en 2029.",
+  },
+  {
+    matiere: "canettes",
+    room: "cafetaria",
+    text: "Dans le cadre de la loi de Transition Energétique, les entreprises ont l'obligation de séparer les déchets en papier,métal,plastique,verre et bois du reste de leur réutilisation ou de leur recyclage. Toutes les entreprises doivent donc être un mesure de trier les canettes et de confier leur collecte et leur recyclage à une entreprise spécialisée !",
+  },
+  {
+    matiere: "capsules",
     room: "cuisine",
-    text: "cuisine2 ipsum dolor sit amet consectetur adipisicing elit. At veniam fugit labore! Doloribus, reiciendis! Aut consequatur iusto in aperiam error sapiente, sit, cupiditate odio nemo incidunt illum eos recusandae consequuntur?",
+    text: "Depuis le 1er janvier 2012,les personnes qui produisent ou détiennent une quantité importante de biodéchets ont l'obligation de trier ces biodéchets et de les faire valoriser dans les filières adaptées. La géneéralistion de ce tri à la source est prévue d'ici 2025 pour tous les producteurs de déchets en France. Objectif: que chaque citoyen ait à sa dispositon une solution lui permettant de ne pas jeter ses biodéchets dans les ordures ménagères résiduelles. ",
   },
   {
+    matiere: "carton",
+    room: "bureau",
+    text: "Les cartons peuvent aussi être réemployés ! Depuis 2012, l'association Carton Plein 75 réemploi des milliers de cartons usagés et réalise des centaines de deéménagements...uniquement à vélo et avec des personnes en situation de grande exclusion.",
+  },
+  {
+    matiere: "equipements electriques et electroniques",
+    room: "reunion",
+    text: "La loi AGEC comporte de nombreuses mesures qui renforcent et étendent la responsabilité élargie des producteurs de la filière DEEE. Ces mesures concernent à la fois les EEE(en tant que produits) et les DEEE(en tant que déchets). Par exemple, l'indice de réparabilité et de durabilité, garanties logicielles,interdiction de toute technique visant à rendre impossible la réparation ou le reconditionnement d'un appareil etc.",
+  },
+  {
+    matiere: "goblets en carton",
     room: "cuisine",
-    text: "cuisine3 ipsum dolor sit amet consectetur adipisicing elit. At veniam fugit labore! Doloribus, reiciendis! Aut consequatur iusto in aperiam error sapiente, sit, cupiditate odio nemo incidunt illum eos recusandae consequuntur?",
+    text: "Le bon geste pour trier les gobelets en carton : il faut bien les vider. Si vous consommez à doomicile, vous pouvez trier votre gobelet dans le bac qui accueille les emballages en papeir-carton. Vous consommez dans la rue ou dans un lieu ? Triez-le dans les bacs de tri disponibles. Il a un couvercle et une paille en plastique ? Séparez-les pour les jeter avec les ordures ménagères.",
   },
   {
-    room: "cafetaria",
-    text: "osjogijipgs pofjsjd!",
+    matiere: "masques jetables",
+    room: "openspace",
+    text: "Le ministère de la santé a formulé des recommandations quant à la gestion des déchets en entreprise. Il est recommandé de prévoir des poubelles à pédale pour les lingettes,mouchoirs,gants à usage unique souillés, coiffes et masques afin de les isoler du reste des déchets et de limiter au maximum les risques de contamination. S'il n'est pas possible de mettre en place des équipements specifiquement dédiés à la collecte des déchets infectieux, ces derniers doivent être intégrés au tout-venant, l'essentiel étant de bien préserver les déchets recyclables qui sont manipulés par des agents de collecte de tri ",
   },
   {
-    room: "cafetaria",
-    text: "cafetaria2 osjogijipgs pofjsjd!",
-  },
-  {
-    room: "cafetaria",
-    text: "cafetaria3 osjogijipgs pofjsjd!",
+    matiere:"cigarettes",
+    room: "openspace",
+    text: "La loi française anti-gaspillage pour une économie circulaire de 2020 stipule la création d'une filière pollueur-payeur pour les cigarettes. Autrement dit,à partir de 2023, les fabricants de cigarettes devront verser 2,08 euros par an et par habitant aux communes de plus de 50.000 habitants, 1,08 euro pour les communes comptant entre 50.000 habitants et 50 centimes pour les plus petites.",
   },
   {
     room: "cafetaria",
@@ -47,28 +57,5 @@ export const tricks = [
     room: "openspace",
     text: "openspace 25% des documents sont jetés 5 minutes après leur impréssion ! Pour limiter le gaspillage, pensez à limiter le nombre d’imprimantes et à bien les paramétrer pour qu'elles impriment en recto-verso par défaut. Installer des bac à brouillons pour récupérer les impréssions et pensez à utiliser du papier recyclé !",
   },
-  {
-    room: "openspace",
-    text: "openspace2 25% des documents sont jetés 5 minutes après leur impréssion ! Pour limiter le gaspillage, pensez à limiter le nombre d’imprimantes et à bien les paramétrer pour qu'elles impriment en recto-verso par défaut. Installer des bac à brouillons pour récupérer les impréssions et pensez à utiliser du papier recyclé !",
-  },
-  {
-    room: "openspace",
-    text: "openspace3 25% des documents sont jetés 5 minutes après leur impréssion ! Pour limiter le gaspillage, pensez à limiter le nombre d’imprimantes et à bien les paramétrer pour qu'elles impriment en recto-verso par défaut. Installer des bac à brouillons pour récupérer les impréssions et pensez à utiliser du papier recyclé !",
-  },
-  {
-    room: "reunion",
-    text: "reu1 Lorem ipsum dolor sit amet consectetur adipisicing elit. At veniam fugit labore! Doloribus, reiciendis! Aut consequatur iusto in aperiam error sapiente, sit, cupiditate odio nemo incidunt illum eos recusandae consequuntur?",
-  },
-  {
-    room: "reunion",
-    text: "reu2 Lorem ipsum dolor sit amet consectetur adipisicing elit. At veniam fugit labore! Doloribus, reiciendis! Aut consequatur iusto in aperiam error sapiente, sit, cupiditate odio nemo incidunt illum eos recusandae consequuntur?",
-  },
-  {
-    room: "reunion",
-    text: "reu3 Lorem ipsum dolor sit amet consectetur adipisicing elit. At veniam fugit labore! Doloribus, reiciendis! Aut consequatur iusto in aperiam error sapiente, sit, cupiditate odio nemo incidunt illum eos recusandae consequuntur?",
-  },
-  {
-    room: "cafetaria",
-    text: "cafet osjogijipgs pofjsjd!",
-  },
+
 ]
