c2e4093cbcd7d91ef9a1f4ad1c323b105f39a6be
Loïc Torres <loic.torresg@gmail.com>
2022-12-15 17:01:44 +0100

    removed bundle analyzer

--- app/page.jsx
+++ app/page.jsx
@@ -57,7 +57,7 @@ export default function Page({}) {
       <div className="px-24 py-3 relative">
         <Image priority={true} src={Logo} alt="logo.png" />
       </div>
-      <h1 className={`text-3xl text-center my-3 text-sans ${edo.className}`}>Adoptons les bons gestes</h1>
+      <h1 className={`text-3xl text-center my-3 text-sans text-secondary-light ${edo.className}`}>Adoptons les bons gestes</h1>
       <div className="grid grid-cols-2 grid-rows-2 p-2 gap-y-5 ">
         {sections.map(({ name, SrcActive, SrcInactive, Src, url }, i) => (
           <Link
--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
@@ -17,4 +14,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withBundleAnalyzer(nextConfig);
+module.exports = nextConfig;
