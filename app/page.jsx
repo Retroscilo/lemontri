eb0bd7e0234125180fa342e81216683a0aa30a52
Loïc Torres <loic.torresg@gmail.com>
2022-12-15 17:51:34 +0100

    modified window

--- app/page.jsx
+++ app/page.jsx
@@ -25,6 +25,7 @@ export default function Page({}) {
   };
   const [sections, setSections] = useState(null);
   useEffect(() => {
+    if (!localStorage) return;
     setSections([
       {
         name: "Au bureau",
