45ce11ddca434f1d463dcb83524cca4538905b5b
Mon Macbook <wendykrwn@gmail.com>
2022-12-15 12:06:31 +0100

    add astuces

--- app/entreprise/page.jsx
+++ app/entreprise/page.jsx
@@ -7,6 +7,7 @@ import kitchen from '/public/salles/Shop.png'
 import reunion from '/public/salles/Ecole.png'
 import Popup from "@/components/popup"
 import { useState, useEffect } from "react"
+import { tricks } from "constantes/tricks"
 // import  from '/public/salles/Cantine.png';
 export default function Page({ }) {
   const [currentTrick, setCurrentTrick] = useState({})
@@ -40,80 +41,6 @@ export default function Page({ }) {
     },
   ]
 
-  const tricks = [
-    {
-      room: "bureau",
-      text: "25% des documents sont jetés 5 minutes après leur impréssion ! Pour limiter le gaspillage, pensez à limiter le nombre d’imprimantes et à bien les paramétrer pour qu'elles impriment en recto-verso par défaut. Installer des bac à brouillons pour récupérer les impréssions et pensez à utiliser du papier recyclé !",
-    },
-    {
-      room: "bureau",
-      text: "Bureau 2 hehe 25% des documents sont jetés 5 minutes après leur impréssion ! Pour limiter le gaspillage, pensez à limiter le nombre d’imprimantes et à bien les paramétrer pour qu'elles impriment en recto-verso par défaut. Installer des bac à brouillons pour récupérer les impréssions et pensez à utiliser du papier recyclé !",
-    },
-    {
-      room: "bureau",
-      text: "Bureau 3 hehe 25% des documents sont jetés 5 minutes après leur impréssion ! Pour limiter le gaspillage, pensez à limiter le nombre d’imprimantes et à bien les paramétrer pour qu'elles impriment en recto-verso par défaut. Installer des bac à brouillons pour récupérer les impréssions et pensez à utiliser du papier recyclé !",
-    },
-    {
-      room: "bureau",
-      text: "Bureau 4 hehe 25% des documents sont jetés 5 minutes après leur impréssion ! Pour limiter le gaspillage, pensez à limiter le nombre d’imprimantes et à bien les paramétrer pour qu'elles impriment en recto-verso par défaut. Installer des bac à brouillons pour récupérer les impréssions et pensez à utiliser du papier recyclé !",
-    },
-    {
-      room: "cuisine",
-      text: "cuisine ipsum dolor sit amet consectetur adipisicing elit. At veniam fugit labore! Doloribus, reiciendis! Aut consequatur iusto in aperiam error sapiente, sit, cupiditate odio nemo incidunt illum eos recusandae consequuntur?",
-    },
-    {
-      room: "cuisine",
-      text: "cuisine2 ipsum dolor sit amet consectetur adipisicing elit. At veniam fugit labore! Doloribus, reiciendis! Aut consequatur iusto in aperiam error sapiente, sit, cupiditate odio nemo incidunt illum eos recusandae consequuntur?",
-    },
-    {
-      room: "cuisine",
-      text: "cuisine3 ipsum dolor sit amet consectetur adipisicing elit. At veniam fugit labore! Doloribus, reiciendis! Aut consequatur iusto in aperiam error sapiente, sit, cupiditate odio nemo incidunt illum eos recusandae consequuntur?",
-    },
-    {
-      room: "cafetaria",
-      text: "osjogijipgs pofjsjd!",
-    },
-    {
-      room: "cafetaria",
-      text: "cafetaria2 osjogijipgs pofjsjd!",
-    },
-    {
-      room: "cafetaria",
-      text: "cafetaria3 osjogijipgs pofjsjd!",
-    },
-    {
-      room: "cafetaria",
-      text: "cafetaria4 osjogijipgs pofjsjd!",
-    },
-    {
-      room: "openspace",
-      text: "openspace 25% des documents sont jetés 5 minutes après leur impréssion ! Pour limiter le gaspillage, pensez à limiter le nombre d’imprimantes et à bien les paramétrer pour qu'elles impriment en recto-verso par défaut. Installer des bac à brouillons pour récupérer les impréssions et pensez à utiliser du papier recyclé !",
-    },
-    {
-      room: "openspace",
-      text: "openspace2 25% des documents sont jetés 5 minutes après leur impréssion ! Pour limiter le gaspillage, pensez à limiter le nombre d’imprimantes et à bien les paramétrer pour qu'elles impriment en recto-verso par défaut. Installer des bac à brouillons pour récupérer les impréssions et pensez à utiliser du papier recyclé !",
-    },
-    {
-      room: "openspace",
-      text: "openspace3 25% des documents sont jetés 5 minutes après leur impréssion ! Pour limiter le gaspillage, pensez à limiter le nombre d’imprimantes et à bien les paramétrer pour qu'elles impriment en recto-verso par défaut. Installer des bac à brouillons pour récupérer les impréssions et pensez à utiliser du papier recyclé !",
-    },
-    {
-      room: "reunion",
-      text: "reu1 Lorem ipsum dolor sit amet consectetur adipisicing elit. At veniam fugit labore! Doloribus, reiciendis! Aut consequatur iusto in aperiam error sapiente, sit, cupiditate odio nemo incidunt illum eos recusandae consequuntur?",
-    },
-    {
-      room: "reunion",
-      text: "reu2 Lorem ipsum dolor sit amet consectetur adipisicing elit. At veniam fugit labore! Doloribus, reiciendis! Aut consequatur iusto in aperiam error sapiente, sit, cupiditate odio nemo incidunt illum eos recusandae consequuntur?",
-    },
-    {
-      room: "reunion",
-      text: "reu3 Lorem ipsum dolor sit amet consectetur adipisicing elit. At veniam fugit labore! Doloribus, reiciendis! Aut consequatur iusto in aperiam error sapiente, sit, cupiditate odio nemo incidunt illum eos recusandae consequuntur?",
-    },
-    {
-      room: "cafetaria",
-      text: "cafet osjogijipgs pofjsjd!",
-    },
-  ]
 
   function getRandomInt(min, max) {
     min = Math.ceil(min)
---
+++ constantes/tricks.js
@@ -0,0 +1,74 @@
+export const tricks = [
+  {
+    room: "bureau",
+    text: "25% des documents sont jetés 5 minutes après leur impréssion ! Pour limiter le gaspillage, pensez à limiter le nombre d’imprimantes et à bien les paramétrer pour qu'elles impriment en recto-verso par défaut. Installer des bac à brouillons pour récupérer les impréssions et pensez à utiliser du papier recyclé !",
+  },
+  {
+    room: "bureau",
+    text: "Bureau 2 hehe 25% des documents sont jetés 5 minutes après leur impréssion ! Pour limiter le gaspillage, pensez à limiter le nombre d’imprimantes et à bien les paramétrer pour qu'elles impriment en recto-verso par défaut. Installer des bac à brouillons pour récupérer les impréssions et pensez à utiliser du papier recyclé !",
+  },
+  {
+    room: "bureau",
+    text: "Bureau 3 hehe 25% des documents sont jetés 5 minutes après leur impréssion ! Pour limiter le gaspillage, pensez à limiter le nombre d’imprimantes et à bien les paramétrer pour qu'elles impriment en recto-verso par défaut. Installer des bac à brouillons pour récupérer les impréssions et pensez à utiliser du papier recyclé !",
+  },
+  {
+    room: "bureau",
+    text: "Bureau 4 hehe 25% des documents sont jetés 5 minutes après leur impréssion ! Pour limiter le gaspillage, pensez à limiter le nombre d’imprimantes et à bien les paramétrer pour qu'elles impriment en recto-verso par défaut. Installer des bac à brouillons pour récupérer les impréssions et pensez à utiliser du papier recyclé !",
+  },
+  {
+    room: "cuisine",
+    text: "cuisine ipsum dolor sit amet consectetur adipisicing elit. At veniam fugit labore! Doloribus, reiciendis! Aut consequatur iusto in aperiam error sapiente, sit, cupiditate odio nemo incidunt illum eos recusandae consequuntur?",
+  },
+  {
+    room: "cuisine",
+    text: "cuisine2 ipsum dolor sit amet consectetur adipisicing elit. At veniam fugit labore! Doloribus, reiciendis! Aut consequatur iusto in aperiam error sapiente, sit, cupiditate odio nemo incidunt illum eos recusandae consequuntur?",
+  },
+  {
+    room: "cuisine",
+    text: "cuisine3 ipsum dolor sit amet consectetur adipisicing elit. At veniam fugit labore! Doloribus, reiciendis! Aut consequatur iusto in aperiam error sapiente, sit, cupiditate odio nemo incidunt illum eos recusandae consequuntur?",
+  },
+  {
+    room: "cafetaria",
+    text: "osjogijipgs pofjsjd!",
+  },
+  {
+    room: "cafetaria",
+    text: "cafetaria2 osjogijipgs pofjsjd!",
+  },
+  {
+    room: "cafetaria",
+    text: "cafetaria3 osjogijipgs pofjsjd!",
+  },
+  {
+    room: "cafetaria",
+    text: "cafetaria4 osjogijipgs pofjsjd!",
+  },
+  {
+    room: "openspace",
+    text: "openspace 25% des documents sont jetés 5 minutes après leur impréssion ! Pour limiter le gaspillage, pensez à limiter le nombre d’imprimantes et à bien les paramétrer pour qu'elles impriment en recto-verso par défaut. Installer des bac à brouillons pour récupérer les impréssions et pensez à utiliser du papier recyclé !",
+  },
+  {
+    room: "openspace",
+    text: "openspace2 25% des documents sont jetés 5 minutes après leur impréssion ! Pour limiter le gaspillage, pensez à limiter le nombre d’imprimantes et à bien les paramétrer pour qu'elles impriment en recto-verso par défaut. Installer des bac à brouillons pour récupérer les impréssions et pensez à utiliser du papier recyclé !",
+  },
+  {
+    room: "openspace",
+    text: "openspace3 25% des documents sont jetés 5 minutes après leur impréssion ! Pour limiter le gaspillage, pensez à limiter le nombre d’imprimantes et à bien les paramétrer pour qu'elles impriment en recto-verso par défaut. Installer des bac à brouillons pour récupérer les impréssions et pensez à utiliser du papier recyclé !",
+  },
+  {
+    room: "reunion",
+    text: "reu1 Lorem ipsum dolor sit amet consectetur adipisicing elit. At veniam fugit labore! Doloribus, reiciendis! Aut consequatur iusto in aperiam error sapiente, sit, cupiditate odio nemo incidunt illum eos recusandae consequuntur?",
+  },
+  {
+    room: "reunion",
+    text: "reu2 Lorem ipsum dolor sit amet consectetur adipisicing elit. At veniam fugit labore! Doloribus, reiciendis! Aut consequatur iusto in aperiam error sapiente, sit, cupiditate odio nemo incidunt illum eos recusandae consequuntur?",
+  },
+  {
+    room: "reunion",
+    text: "reu3 Lorem ipsum dolor sit amet consectetur adipisicing elit. At veniam fugit labore! Doloribus, reiciendis! Aut consequatur iusto in aperiam error sapiente, sit, cupiditate odio nemo incidunt illum eos recusandae consequuntur?",
+  },
+  {
+    room: "cafetaria",
+    text: "cafet osjogijipgs pofjsjd!",
+  },
+]
